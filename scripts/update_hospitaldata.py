35374534fb300bb1a8d6eda446f27dffe405fa53
Tianning Li <ltianningli@gmail.com>
2020-12-29 14:43:38 -0800

    fix hospital data scripts

--- scripts/update_hospitaldata.py
+++ scripts/update_hospitaldata.py
@@ -30,13 +30,13 @@ def get_data():
     return df
 
 df = get_data()
-# df.to_csv('data/hospital_data.csv', index=False)
-# base_path = 'time_series_data/'
-# for i in sorted(set(df['collection_date'].to_list())):
-#     dir_path = base_path + i
-#     if not os.path.isdir(dir_path):
-#         os.mkdir(dir_path)
-#     df[df['collection_date'] == i].to_csv(dir_path + '/hospital_data.csv', index=False)
+df.to_csv('data/hospital_data.csv', index=False)
+base_path = 'time_series_data/'
+for i in sorted(set(df['collection_date'].to_list())):
+    dir_path = base_path + i
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+    df[df['collection_date'] == i].to_csv(dir_path + '/hospital_data.csv', index=False)
 
 state_list = list(set(list(df['state'].values)))
 meta = {
